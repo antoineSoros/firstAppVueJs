b4c199ec159af1bfd8e892eb7c272f73372e0fac
backaligth <antoine.soros@orange.fr>
2018-12-31 00:13:06 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+    product:'SOCKS',
+    description: 'A pair of warm, fuzzy socks',
+    image:'./assets/vmSocks-green-onWhite.jpg',
+    altText:'a pair of socks',
+    inStock: true,
+    onSale: true,
+    details:["80% cotton","20% polyester","Gender-neutral "]
+}
+});
